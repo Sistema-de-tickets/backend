b9accf76cc4199f990f443d4367b02b9019b1df5
Neft <gersonneftaly1@gmail.com>
2026-09-15 20:09:49 -0600

    supabase auth implementado en front por comodidad JAJAJJAA

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
 // Este solo es un ejemplo de como meter las rutas
 
-import authRoutes from "../modules/auth/auth.routes";
-
 const router = Router();
-
-router.use("/auth", authRoutes);
 
 export default router;
